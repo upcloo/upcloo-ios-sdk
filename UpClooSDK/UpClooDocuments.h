ea4b3b965ebd767282332e5bc0aba513c5b6b754
Walter Dal Mut <walter.dalmut@gmail.com>
2012-03-14 22:38:31 +0100

    Init parse work

--- UpClooSDK/UpClooDocuments.h
+++ UpClooSDK/UpClooDocuments.h
@@ -8,8 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
-@interface UpClooDocuments : NSObject
+@class UpClooDocument;
+
+@interface UpClooDocuments : NSObject <NSXMLParserDelegate> {
+    NSMutableArray *documents;
+    NSMutableData *xmlData;
+    
+    UpClooDocument *document;
+    NSString *actualElement;
+}
+
+@property (nonatomic, retain) NSMutableArray *documents;
+@property (nonatomic, retain) NSMutableData *xmlData;
+@property (nonatomic, retain) UpClooDocument *document;
+@property (nonatomic, retain) NSString *actualElement;
 
 - (id)initWithNSMutableData: (NSMutableData *)data;
+- (void)start;
 
 @end
--- UpClooSDK/UpClooDocuments.m
+++ UpClooSDK/UpClooDocuments.m
@@ -7,17 +7,75 @@
 //
 
 #import "UpClooDocuments.h"
+#import "UpClooDocument.h"
+#import "UpClooSDK.h"
 
 @implementation UpClooDocuments
+
+@synthesize documents, document;
+@synthesize xmlData;
+@synthesize actualElement;
 
 - (id)initWithNSMutableData:(NSMutableData *)data
 {
     if (self = [super init])
     {
-        //init...
+        self.xmlData = data;
     }
     
     return self;
+}
+
+- (void)start
+{
+    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:self.xmlData];
+    parser.delegate = self;
+    
+    [parser setShouldProcessNamespaces:NO];
+    [parser setShouldReportNamespacePrefixes:NO];
+    [parser setShouldResolveExternalEntities:NO];
+    [parser parse];
+
+}
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
+{
+    if ([elementName isEqual:@"doc"]) {
+        self.document = [[UpClooDocument alloc] init];
+        actualElement = @"doc";
+    } else if ([elementName isEqual:@"title"]) {
+        actualElement = @"title";
+    }
+}
+
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+    if ([elementName isEqual:@"doc"]) {
+        [documents addObject:document];
+
+        actualElement = nil;
+        document = nil;
+    }
+}
+
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+    if ([actualElement isEqual:@"title"]) {
+        document.title = string;
+    }
+}
+
+- (void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError
+{
+    UpClooSDK *manager = [UpClooSDK sharedManager];
+    [manager.delegate upclooUnableToGetContentsWithMessage:[parseError localizedDescription]];
+}
+
+- (void)parserDidEndDocument:(NSXMLParser *)parser
+{
+    //handle ends
+    UpClooSDK *manager = [UpClooSDK sharedManager];
+    [manager.delegate upclooContentsReady:self];
 }
 
 @end
